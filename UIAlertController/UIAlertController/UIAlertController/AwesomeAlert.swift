edb92be7f979db262969d20f215baa7d71c8fddd
BobDeKort <dekort.bob101@gmail.com>
2017-02-16 14:36:02 -0800

    can change Background color, title font and color, message font and color, set all actions text colors and individual actions

---
+++ UIAlertController/UIAlertController/UIAlertController/AwesomeAlert.swift
@@ -0,0 +1,107 @@
+//
+//  AwesomeAlert.swift
+//  UIAlertController
+//
+//  Created by Bob De Kort on 2/16/17.
+//  Copyright © 2017 Bob De Kort. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIAlertController {
+    
+    func addBackgroundColor(color: UIColor) {
+        let backgroundView = self.view.subviews.first
+        if let backView = backgroundView {
+            backView.layer.cornerRadius = 15.0
+            backView.backgroundColor = color
+        }
+        
+    }
+    
+    func setTintColor(color: UIColor) {
+        self.view.tintColor = color
+    }
+    
+    func addVisualeffects(blurEffect: UIBlurEffect?, vibrancyEffect: UIVibrancyEffect?) {
+        if let visualEffectView = self.view.searchVisualEffectsSubview()
+        {
+            if let blur = blurEffect {
+                visualEffectView.effect = blur
+            }
+            
+            if let vibrancy = vibrancyEffect {
+                visualEffectView.effect = vibrancy
+            }
+        }
+    }
+    
+    func setAttributedTitle(title: String, color: UIColor?, font: UIFont?){
+        
+        let mutableString = NSMutableAttributedString(string: title)
+        if let font = font {
+            mutableString.addAttribute(NSFontAttributeName, value: font, range: NSRange(location: 0, length: title.characters.count))
+        }
+        
+        if let color = color {
+            mutableString.addAttribute(NSForegroundColorAttributeName, value: color, range: NSRange(location: 0, length: title.characters.count))
+        }
+        
+        self.setValue(mutableString, forKey: "attributedTitle")
+    }
+    
+    func setAttributedMessage(message: String, color: UIColor?, font: UIFont?) {
+        
+        let mutableString = NSMutableAttributedString(string: message)
+        if let font = font {
+            mutableString.addAttribute(NSFontAttributeName, value: font, range: NSRange(location: 0, length: message.characters.count))
+        }
+        
+        if let color = color {
+            mutableString.addAttribute(NSForegroundColorAttributeName, value: color, range: NSRange(location: 0, length: message.characters.count))
+        }
+        
+        self.setValue(mutableString, forKey: "attributedMessage")
+    }
+    
+    func setAllActionsTextColors(color: UIColor){
+        for action in actions {
+            action.setValue(color, forKey: "titleTextColor")
+        }
+    }
+    
+    func setActionTextColor(actionTitle: String, color: UIColor){
+        for action in actions {
+            if action.title == actionTitle {
+                action.setValue(color, forKey: "titleTextColor")
+            }
+        }
+    }
+    
+    
+    
+}
+
+extension UIView
+{
+    func searchVisualEffectsSubview() -> UIVisualEffectView?
+    {
+        if let visualEffectView = self as? UIVisualEffectView
+        {
+            return visualEffectView
+        }
+        else
+        {
+            for subview in subviews
+            {
+                if let found = subview.searchVisualEffectsSubview()
+                {
+                    return found
+                }
+            }
+        }
+        
+        return nil
+    }
+}
---
+++ UIAlertController/UIAlertController/UIAlertController/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  UIAlertController
+//
+//  Created by Bob De Kort on 2/16/17.
+//  Copyright © 2017 Bob De Kort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let alertController = UIAlertController(title: "Simple", message: "Simple alertView demo with Cancel and Ok.", preferredStyle: UIAlertControllerStyle.alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) { (result : UIAlertAction) -> Void in
+            print("Cancel")
+        }
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
+            print("OK")
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        
+//        alertController.addBackgroundColor(color: .red)
+        
+//        alertController.setButtonTextColor(color: .white)
+        
+//        let blur = UIBlurEffect(style: .regular)
+//        alertController.addVisualeffects(blurEffect: blur, vibrancyEffect: nil)
+        
+        
+//        alertController.setAttributedTitle(title: "Testing", color: .red, font: UIFont(name: "Georgia", size: 18.0))
+        
+//        alertController.setAttributedMessage(message: alertController.message!, color: .red, font: UIFont(name: "Georgia", size: 16.0))
+        
+//        alertController.setAllButtonTextColor(color: .red)
+        
+//        alertController.setActionTextColor(actionTitle: "Cancel", color: .red)
+        
+        
+        
+        present(alertController, animated: true, completion: nil)
+
+        
+        print(alertController.actions)
+    }
+}
+
