e38fddc45c2b99ddcb4c0c76c34a6d74849d4689
tlambrou <lambrou@gmail.com>
2017-02-19 20:20:29 -0800

    Added IBDesignable and IBInspectable elements

--- HexView/HexView/HexView.swift
+++ HexView/HexView/HexView.swift
@@ -8,22 +8,142 @@
 
 import UIKit
 
-class HexView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+@IBDesignable class HexView: UIView {
+  
+  /*
+   // Only override draw() if you perform custom drawing.
+   // An empty implementation adversely affects performance during animation.
+   override func draw(_ rect: CGRect) {
+   // Drawing code
+   }
+   */
+  
+  @IBInspectable var color: UIColor = UIColor.cyan {didSet{layoutSubviews()}}
+  
+  @IBInspectable var strokeWidth: CGFloat = 3 {didSet{layoutSubviews()}}
+  
+  @IBInspectable var strokeColor: UIColor = UIColor.magenta {didSet{layoutSubviews()}}
+  
+  var shapeLayer = CAShapeLayer()
+  var path = UIBezierPath()
   
   override init(frame: CGRect) {
     super.init(frame: frame)
+  
   }
   
   required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
+    super.init(coder: aDecoder)
+  
   }
-
+  
+  override func layoutSubviews() {
+    setup()
+  }
+  
+  func setup() {
+    
+    
+    
+    // Geometry
+    let sides = 6
+    let cx = self.bounds.width / 2
+    let cy = self.bounds.height / 2
+    let c = self.center
+    let r = cy < cx ? cy : cx
+    
+    let start = CGPoint(x: cx, y: cy + r)
+    path.move(to: start)
+    
+    // Looping around the hexagon
+    for i in 0 ... sides {
+      
+      let t = CGFloat(M_PI * 2) / CGFloat(sides) * CGFloat(i)
+      let x = sin(t) * r + cx
+      let y = cos(t) * r + cy
+      path.addLine(to: CGPoint(x: x, y: y))
+    }
+    
+    path.close()
+    shapeLayer.fillColor = color.cgColor
+    shapeLayer.path = path.cgPath
+    self.layer.addSublayer(shapeLayer)
+    self.layer.backgroundColor = UIColor.clear.cgColor
+    
+    shapeLayer.lineWidth = strokeWidth
+    shapeLayer.strokeColor = strokeColor.cgColor
+  
+  }
+  
+  func animation1 () {
+    
+    self.shapeLayer.strokeStart = 0
+    self.shapeLayer.strokeEnd = 0
+    UIView.animate(withDuration: 3, delay: 6, options: [], animations: {
+      self.shapeLayer.strokeStart = 0
+      self.shapeLayer.strokeEnd = 1/6
+    }, completion: nil)
+    
+    
+//    UIView.animateKeyframes(withDuration: 7, delay: 0.5, options: [], animations: {
+//      UIView.addKeyframe(withRelativeStartTime: 1/6, relativeDuration: 0.2, animations: {
+//        self.shapeLayer.strokeStart = 0
+//        self.shapeLayer.strokeEnd = 1/6
+//      })
+//      UIView.addKeyframe(withRelativeStartTime: 1/6, relativeDuration: 0.2, animations: {
+//        self.shapeLayer.strokeStart = 1/6
+//        self.shapeLayer.strokeEnd = 2/6
+//      })
+//      UIView.addKeyframe(withRelativeStartTime: 1/6, relativeDuration: 0.2, animations: {
+//        self.shapeLayer.strokeStart = 2/6
+//        self.shapeLayer.strokeEnd = 3/6
+//      })
+//      UIView.addKeyframe(withRelativeStartTime: 1/6, relativeDuration: 0.2, animations: {
+//        self.shapeLayer.strokeStart = 3/6
+//        self.shapeLayer.strokeEnd = 4/6
+//      })
+//      UIView.addKeyframe(withRelativeStartTime: 1/6, relativeDuration: 0.2, animations: {
+//        self.shapeLayer.strokeStart = 4/6
+//        self.shapeLayer.strokeEnd = 5/6
+//      })
+//      UIView.addKeyframe(withRelativeStartTime: 1/6, relativeDuration: 0.2, animations: {
+//        
+//        self.shapeLayer.strokeStart = 5/6
+//        self.shapeLayer.strokeEnd = 6/6
+//      })
+//      
+//    }, completion: nil)
+    
+  }
+  
+  func strokeSide1 () {
+    shapeLayer.strokeStart = 0
+    shapeLayer.strokeEnd = 1/6
+  }
+  
+  func strokeSide2 () {
+    shapeLayer.strokeStart = 1/6
+    shapeLayer.strokeEnd = 2/6
+  }
+  
+  func strokeSide3 () {
+    shapeLayer.strokeStart = 2/6
+    shapeLayer.strokeEnd = 3/6
+  }
+  
+  func strokeSide4 () {
+    shapeLayer.strokeStart = 3/6
+    shapeLayer.strokeEnd = 4/6
+  }
+  
+  func strokeSide5 () {
+    shapeLayer.strokeStart = 4/6
+    shapeLayer.strokeEnd = 5/6
+  }
+  
+  func strokeSide6 () {
+    shapeLayer.strokeStart = 5/6
+    shapeLayer.strokeEnd = 6/6
+  }
+  
 }
--- HexView/HexView/HexView/ViewController.swift
+++ HexView/HexView/HexView/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var hexView: HexView!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    hexView.shapeLayer.strokeStart = 0
+    hexView.shapeLayer.strokeEnd = 0
+    UIView.animate(withDuration: 1, delay: 1, options: [], animations: {
+      self.hexView.shapeLayer.strokeStart = 0
+      self.hexView.shapeLayer.strokeEnd = 0.42
+    }, completion: nil)
+
+    
+//    
+//    let frame = CGRect(x: 40, y: 40, width: 200, height: 200)
+//    
+//    let hex = HexView(frame: frame)
+//    hex.layer.backgroundColor = UIColor.cyan.cgColor
+//    hex.center = view.center
+//    view.addSubview(hex)
+    
+    
   }
 
   override func didReceiveMemoryWarning() {
